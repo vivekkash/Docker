c5ccd8eb7121447d5346077adb6799c8913cec1e
vivek <vivekkashyap97@gmail.com>
2020-08-21 06:23:40 +0530

    Pushing another method of lauching docker container well know by docker-compose

--- docker-php/src/index.php
+++ docker-php/src/index.php
@@ -32,7 +32,7 @@
 		<!--  -->
 		<div class="size1 flex-col-c-m p-l-15 p-r-15 p-t-50 p-b-50">
 			<h3 class="l1-txt1 txt-center p-b-25">
-				Coming Soon - Docker Demo
+				Coming Soon - Docker Demo 2
 			</h3>
 
 			<p class="m2-txt1 txt-center p-b-48">
